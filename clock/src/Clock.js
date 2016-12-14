b1ca90c3fba453144c4a5012d6b73071de4de348
Luka Mis <luka.mis@gmail.com>
2016-12-14 15:00:45 +0100

    Clock update. State is stored and rotation goes always clockwise.

--- clock/src/Clock.js
+++ clock/src/Clock.js
@@ -26,22 +26,22 @@
 
 
 
-let Clock = function(configObject) {
+const Clock = (configObject) => {
   
   'use strict';
-  let instance = {};
-
+  const instance = {};
   let handsData = [];
+  let handsDataOld = [];
 
   let clockId;
 
 
-  const makeAClock = function(configObject) {
+  const makeAClock = (configObject) => {
 
-    let date = new Date();
-    let seconds = date.getSeconds();
-    let minutes = date.getMinutes();
-    let hours = date.getHours();
+    const date = new Date();
+    const seconds = date.getSeconds();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
 
     clockId = configObject.id;
 
@@ -82,8 +82,10 @@ let Clock = function(configObject) {
   };
 
 
-  const setClockTime = function setClockTime(data) {
+  const setClockTime = (data) => {
     // Create an object with each hand and it's angle in degrees
+
+    $('#' + clockId + ' div[data-type="hours"]').one('transitionend', onHandTransitionComplete);
 
     if(data.hours === undefined) {
       data.hours = 0;
@@ -97,33 +99,166 @@ let Clock = function(configObject) {
       data.seconds = 0;
     }
 
+
+    // if(data.hours > 360) {
+    //   data.hours -= 360;
+    // }
+    
+    // if(data.minutes > 360 ) {
+    //   data.minutes -= 360;
+    // }
+    
+    // if(data.seconds > 360) {
+    //   data.seconds -= 360;
+    // }
+
+
+    // if( handsDataOld[0] ) {
+    //   console.log(' ');
+    //   console.log( 'handsDataOld[0].angle ', handsDataOld[0].angle  );
+    //   console.log( '(data.hours * 30) + (data.minutes / 2) ', (data.hours * 30) + (data.minutes / 2)  );
+    // }
+
+    // console.log('handsDataOld[0]', handsDataOld[0]);
+
     handsData.length = 0;
+
+    let angleForHour = (data.hours * 30) + (data.minutes / 2);
+    if(angleForHour >= 360) {
+      angleForHour -= 360;
+    }
+    let angleForMinutes = (data.minutes * 6);
+    if(angleForMinutes >= 360) {
+      angleForMinutes -= 360;
+    }
+    let angleForSeconds = (data.seconds * 6);
+    if(angleForSeconds >= 360) {
+      angleForSeconds -= 360;
+    }
+
+
+    if(handsDataOld[0]) {
+      console.log(' ');
+      console.log( 'handsDataOld[0].angle', handsDataOld[0].angle );
+      console.log( 'angleForHour', angleForHour );
+      if(angleForHour < handsDataOld[0].angle) {
+        angleForHour += 360;
+      }
+      if(angleForMinutes < handsDataOld[1].angle) {
+        angleForMinutes += 360;
+      }
+      if(angleForSeconds < handsDataOld[2].angle) {
+        angleForSeconds += 360;
+      }
+
+      // angleForHour += 360;
+      // angleForMinutes += 360;
+      // angleForSeconds += 360;
+    }
+
     handsData = [
       {
         hand: 'hours',
-        angle: (data.hours * 30) + (data.minutes / 2)
+        angle: angleForHour
       },
       {
         hand: 'minutes',
-        angle: (data.minutes * 6)
+        angle: angleForMinutes
       },
       {
         hand: 'seconds',
-        angle: (data.seconds * 6)
+        angle: angleForSeconds
       }
     ];
+
+
+    // console.log( );
+    // handsData.forEach(function(element, index) {
+    //   console.log(element.angle);
+    // });
+
     // Loop through each of these handsData to set their angle
     for (let j = 0; j < handsData.length; j++) {
       let elements = document.getElementById(clockId).querySelectorAll('[data-type="' + handsData[j].hand + '"]');
       for (let k = 0; k < elements.length; k++) {
         elements[k].style.transform = ' translate(-50%) ' + ' ' + 'rotate('+ handsData[j].angle +'deg)';
+        elements[k].style['transition-duration'] = '';
       }
     }
+
+
+    // let angleForHourOld = (data.hours * 30) + (data.minutes / 2);
+    // if(angleForHourOld > 360) {
+    //   angleForHourOld -= 360;
+    // }
+    // let angleForMinutesOld = (data.minutes * 6);
+    // if(angleForMinutesOld > 360) {
+    //   angleForMinutesOld -= 360;
+    // }
+    // let angleForSecondsOld = (data.seconds * 6);
+    // if(angleForSecondsOld > 360) {
+    //   angleForSecondsOld -= 360;
+    // }
+
+    handsDataOld.length = 0;
+    handsDataOld = [
+      {
+        hand: 'hours',
+        angle: angleForHour
+      },
+      {
+        hand: 'minutes',
+        angle: angleForMinutes
+      },
+      {
+        hand: 'seconds',
+        angle: angleForSeconds
+      }
+    ];
+
   };
 
 
-  const getTimeOfDay = function getTimeOfDay() {
-    let date = new Date();
+  const onHandTransitionComplete = (e) => {
+    $('#' + clockId + ' div[data-type="hours"]').off('transitionend', onHandTransitionComplete);
+    $(instance).trigger('onTimeSet', {id:clockId});
+    resetAngles();
+  };
+
+
+  const resetAngles = () => {
+    let hour = handsDataOld[0].angle;
+    if(hour >= 360) {
+      hour -= 360;
+      handsDataOld[0].angle = hour;
+      console.log('hour reset by 360');
+    }
+    let min = handsDataOld[1].angle;
+    if(min >= 360) {
+      min -= 360;
+      handsDataOld[1].angle = min;
+      console.log('min reset by 360');
+    }
+    let sec = handsDataOld[2].angle;
+    if(sec >= 360) {
+      sec -= 360;
+      handsDataOld[2].angle = sec;
+      console.log('sec reset by 360');
+    }
+
+    for (let j = 0; j < handsDataOld.length; j++) {
+      let elements = document.getElementById(clockId).querySelectorAll('[data-type="' + handsDataOld[j].hand + '"]');
+      for (let k = 0; k < elements.length; k++) {
+        elements[k].style.transform = ' translate(-50%) ' + ' ' + 'rotate('+ handsDataOld[j].angle +'deg)';
+        elements[k].style['transition-duration'] = '0s';
+      }
+    }
+
+  };
+
+
+  const getTimeOfDay = () => {
+    const date = new Date();
     return {
       hours: date.getHours(),
       minutes: date.getMinutes(),
@@ -132,7 +267,7 @@ let Clock = function(configObject) {
   };
 
 
-  const init = function init() {
+  const init = () => {
     makeAClock(configObject);
     setClockTime({
       hours: 0,
@@ -145,15 +280,21 @@ let Clock = function(configObject) {
 
 
   /* API */
-  instance.setTime = function(object) {
+  instance.setTime = (object) => {
     setClockTime(object);
   };
-  instance.setCurrentTime = function() {
+  instance.setInitialTime = () => {
+    setClockTime({
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    });
+    $(instance).trigger('onTimeSet', {id:clockId});
+  };
+  instance.setCurrentTime = () => {
     setClockTime(getTimeOfDay());
   };
-  instance.getId = function(object) {
-    return clockId;
-  };
+  instance.getId = () => { return clockId };
 
   return instance;
 };
